68535738b05051e1a025de9cd892f78606d5ed44
Luiza Nurnberg <nurnbergluiza@gmail.com>
2023-04-26 23:23:39 -0300

    style: todos os arquivos foram renomeados conforme a sua função e usuário que esterá utilizando

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
-
 <?php
 // o pg_connect tem um parametro só que é uma string. tavam sendo passados varios
 $dbconn = pg_connect("host=database_service port=5432 dbname=my_db user=my_user password=12345")
-or die('Could not connect');
+    or die('Could not connect');
 echo "Conexão efetuada com sucesso!!";
-?>
