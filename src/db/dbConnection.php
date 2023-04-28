f690efc32d9bd25377b0ac0a1327695646cbfb01
Luiza Nurnberg <nurnbergluiza@gmail.com>
2023-04-27 21:42:37 -0300

    feat: arquivo de conexao do banco, criação do model Produto. Definido o ingles como padrão de código

---
+++ src/db/dbConnection.php
@@ -0,0 +1,11 @@
+<?php
+try {
+    $dbconn = pg_connect("host=database_service port=5432 dbname=my_db user=my_user password=12345");
+
+    if (!$dbconn) {
+        die("Conexão falhou com o Banco de Dados falhou!");
+    }
+    echo "Conexão efetuada com o Banco de Dados efetuada com sucesso!";
+} catch (error) {
+    echo pg_last_error();
+}
